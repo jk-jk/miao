baf5d8772fddcecd9263db0a90aae757adbead27
Jkkkk <378995953@qq.com>
2018-07-18 10:26:00 +0800

    3

--- lodash/jk_jk-lodash.js
+++ lodash/jk_jk-lodash.js
@@ -1,8 +1,34 @@
 var jk_jk = {
   chunk: function(array, size) {
     let res = []
-    for(var i = 0;i < array.length; i+=size){
+    for(let i = 0;i < array.length; i+=size){
       res.push( array.slice(i , i + size))
+    }
+    return res
+  },
+
+  compact: function(array){
+    let ary = array
+    let Ary = []
+    for(let i = 0;i < ary.length;i++){
+      if(ary[i]){
+        Ary.push(ary[i])
+      }
+    }
+    return Ary
+  },
+
+  concat: function(array,...values){
+    var res=[]
+    for (let value of array){
+      res.push(value)
+    }
+    for (let value of values){
+      if(Array.isArray(value)){
+        res.push(...value)
+      } else {
+        res.push(value)
+      }
     }
     return res
   }
