105d753bf4e99856f7f8842848de38c7e5c374b2
Jkkkk <378995953@qq.com>
2018-07-18 09:28:48 +0800

    111

--- lodash/jk_jk-lodash.js
+++ lodash/jk_jk-lodash.js
@@ -1,5 +1,9 @@
 var jk_jk = {
   chunk: function(array, size) {
-
+    let res = []
+    for(var i = 0;i < array.length; i+=size){
+      res.push( array.slice(i , i + size))
+    }
+    return res
   }
 }
